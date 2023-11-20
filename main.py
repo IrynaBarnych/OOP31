4065cba218df78e5818a4fe4e1dba467c56d0581
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-20 20:27:30 +0200

    #property()

--- main.py
+++ main.py
@@ -19,3 +19,34 @@ def summa():
     return sum(range(1, 100000))
 
 print(summa())
+
+#property()
+
+class Circle:
+    def __init__(self, radius):
+        self._radius = radius
+
+    def get_radius(self):
+        print("get_radius")
+        return self._radius
+
+    def set_radius(self, value):
+        print("set_radius")
+        if value < 0:
+            raise ValueError("Значення радіуса не може бути меньше нуля")
+        self._radius = value
+
+    def delete_radius(self):
+        print("delete_radius")
+        del self._radius
+    radius = property(fget=get_radius, fset=set_radius, fdel=delete_radius)
+
+circle = Circle(10)
+#print(circle.get_radius())
+#circle.set_radius(100)
+#print(circle.get_radius())
+print(circle.radius) #fget=get_radius
+circle.radius = 100
+print(circle.radius)
+del circle.radius
+#print(circle.radius)

44b377051554c2d7d07a65a725d5052c7aa960c9
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-20 20:06:22 +0200

    /

--- main.py
+++ main.py
@@ -5,3 +5,17 @@
 # Об'єкт класу Calculator буде функтором, що може
 # викликатися для виконання обраної операції.
 
+class Calculator:
+    #інші операції
+    def divide(self, a, b):
+        if b == 0:
+            raise ZeroDivisionError("Ділити на нуль не можна")
+        return a / b
+    def __call__(self, *args, **kwargs):
+        match args[0]:
+            # додати усі інші операції
+            case "/": return self.divide(args[1], args[2])
+            case _  : return "такої  операції немає"
+
+calcutate = Calculator()
+print(calcutate("/", 4, 2))

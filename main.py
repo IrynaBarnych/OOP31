dcf3615df991db9b92c68d75da6d059f9580ece9
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-20 19:39:16 +0200

    додала завдання

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
-#функтори
-#метод __call__
+# Завдання 1
+# Створіть клас Calculator, який може виконувати
+# операції додавання, віднімання, множення та ділення.
+# Кожна операція буде реалізована як метод класу.
+# Об'єкт класу Calculator буде функтором, що може
+# викликатися для виконання обраної операції.
 
-import time
-class Timer:
-    def __init__(self, func):
-        self.func = func
-
-    def __call__(self, *args, **kwargs):
-        start = time.time()
-        print(f"Викликаємо функцію {self.func.__name__}")
-        result = self.func(*args, **kwargs)
-        end = time.time()
-        print(f"Функція виконувалася {end - start:.5f}" )
-        return result
-
-@Timer
-def summa():
-    return sum(range(1, 100000))
-
-print(summa())

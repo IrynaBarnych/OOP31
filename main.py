628796b7eb9d0f46a8cf74ef1c79a84b907f1e37
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-26 16:39:52 +0200

    додала код варіант 2

--- main.py
+++ main.py
@@ -21,6 +21,41 @@ person_info = Person("Тарас", 33)
 print(person_info.information())
 
 
+#Варіант 2
+
+class Person:
+    def __init__(self, name, age):
+        self._name = None
+        self._age = None
+        self.name = name
+        self.age = age
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not value.isalpha():
+            raise ValueError("Ім'я має складатися лише з букв!")
+        self._name = value
+
+    @property
+    def age(self):
+        return self._age
+
+    @age.setter
+    def age(self, value):
+        if not (0 <= value <= 120):
+            raise ValueError("Вік має бути в діапазоні від 0 до 120 років")
+        self._age = value
+
+    def information(self):
+        return f"Ім'я: {self.name}, Вік: {self.age}"
+
+
+person_info = Person("Віктор", 33)
+print(person_info.information())
 
 
 

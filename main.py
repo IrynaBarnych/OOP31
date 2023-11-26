e6e641c7d49379d6c61d81342d3883e45146c146
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-26 16:38:59 +0200

    додала код

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
-#Завдання 3
-#Створіть клас для представлення відомостей про
-#замовлення. Забезпечте, щоб номер замовлення був
-#тільки для читання за допомогою керованих атрибутів,
-#але його можна було переглядати.
+#Завдання 4
+#Створіть клас для представлення користувача з
+#атрибутами: ім'я та вік. Додайте властивості для
+#валідації віку користувача. Наприклад, вік повинен
+#бути у межах від 0 до 120.
 
-class Order:
-    def __init__(self, order_number, customer_name, total_amount):
-        self._order_number = order_number
-        self.customer_name = customer_name
-        self.total_amount = total_amount
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
-    def get_order_number(self):
-        return self._order_number
+    def information(self):
+        if not self.name.isalpha():
+            raise ValueError("Ім'я має складатися лише з букв!")
+        if not (0 <= self.age <= 120):
+            raise ValueError("Вік має бути в діапазоні від 0 до 120 років")
+        return f"Ім'я: {self.name}, Вік: {self.age}"
 
-order_instance = Order(12345, "John Doe", 100.0)
-print(order_instance.get_order_number())
+
+person_info = Person("Тарас", 33)
+print(person_info.information())
+
 
 
 

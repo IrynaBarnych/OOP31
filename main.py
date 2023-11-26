d98e22c211341944453bd4ad1098e6e16dcde257
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-26 17:28:44 +0200

    додала текст з відео

--- main.py
+++ main.py
@@ -7,5 +7,30 @@
 # зарахувалися тільки при виконанні певних умов
 # (наприклад, якщо гроші на рахунку є).
 
+class BankAccount:
+    def __int__(self, name, balance):
+        self.name = name
+        self.__balance = balance
 
+    def get_balance(self):
+        print('get_balance')
+        return self.__balance
+
+    def set_balance(self, value):
+        print('set balance')
+        is not isinstance(value, (int, float)):
+            raise ValueError('Баланс має бути числом')
+        self.__balance == value
+
+    def delete_balance(self):
+        print('delete balance')
+        del self.__balance
+
+    my_balance = property()
+    my_balance = my_balance.getter(get_balance)
+    my_balance = my_balance.setter(set_balance)
+    my_balance = my_balance.deleter(delete_balance)
+    balance = property(fget_balance,
+                       fset=set_balance,
+                       sdel=delete_balance)
 

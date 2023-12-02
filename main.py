c2ff0463bcef28357f08fe619a8ebf52034b3c6f
IrynaBarnych <iryna.barnych@gmail.com>
2023-12-02 22:02:08 +0200

    додала     @property

--- main.py
+++ main.py
@@ -7,15 +7,37 @@
 class Order:
     def __init__(self, order_number, customer_name, total_amount):
         self._order_number = order_number
-        self.customer_name = customer_name
-        self.total_amount = total_amount
+        self._customer_name = customer_name
+        self._total_amount = total_amount
 
-    def get_order_number(self):
+    @property
+    def order_number(self):
         return self._order_number
+
+    @property
+    def customer_name(self):
+        return self._customer_name
+
+    @property
+    def total_amount(self):
+        return self._total_amount
+
+    @total_amount.setter
+    def total_amount(self, value):
+        if value < 0:
+            raise ValueError("Total amount cannot be negative")
+        self._total_amount = value
 
 
 order_instance = Order(12345, "John Doe", 100.0)
-print(order_instance.get_order_number())
+
+
+print(order_instance.order_number)
+print(order_instance.customer_name)
+print(order_instance.total_amount)
+
+order_instance.total_amount = 150.0
+print(order_instance.total_amount)
 
 
 
